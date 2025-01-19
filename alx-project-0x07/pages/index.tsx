e0f1df13d4a364083efa6126d949cc60431e6bfd
thatboyreegan <reaganronald491@gmail.com>
2025-01-19 20:50:12 +0300

    add: moved a repo

--- alx-project-0x07/pages/index.tsx
+++ alx-project-0x07/pages/index.tsx
@@ -9,7 +9,7 @@ const Home: React.FC = () => {
   return (
     <div className="flex flex-col items-center min-h-screen bg-gray-100 p-4">
       <div className="flex flex-col items-center">
-        <h1 className="text-4xl font-bold mb-2">Image Generation App</h1>
+        <h1 className="text-4xl font-bold text-gray-700 mb-2">Image Generation App</h1>
         <p className="text-lg text-gray-700 mb-4">
           Generate stunning images based on your prompts!
         </p>
